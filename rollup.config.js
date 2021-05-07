79927544ba2eb47895892a33970c6c74ea6e481d
Niklas May <post@niklas-may.de>
2021-05-07 18:33:36 +0200

    prettier and eslint setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,46 @@
-import rimraf from "rimraf";
-import replace from "rollup-plugin-replace";
-import copy from "rollup-plugin-copy";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
+import rimraf from 'rimraf';
+import replace from 'rollup-plugin-replace';
+import copy from 'rollup-plugin-copy';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import { eslint } from 'rollup-plugin-eslint';
 
-rimraf.sync("dist");
+rimraf.sync('dist');
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 const version = pkg.version;
 const isDev = process.env.ROLLUP_WATCH;
 
 export default {
-  input: ["src/scripts/main.js"],
+  input: ['src/scripts/main.js'],
   output: {
-    file: "dist/bundle.js",
-    format: "iife",
+    file: 'dist/bundle.js',
+    format: 'iife',
     sourcemap: true,
   },
   watch: {
-    include: ["src/**"],
+    include: ['src/**'],
   },
   plugins: [
+    eslint(),
     replace({
-      delimiters: ["{{", "}}"],
+      delimiters: ['{{', '}}'],
       version: version,
     }),
     copy({
       targets: [
-        { src: "src/static/*.html", dest: "dist/" },
-        { src: "src/static/images", dest: "dist/images" },
+        { src: 'src/static/*.html', dest: 'dist/' },
+        { src: 'src/static/images', dest: 'dist/images' },
       ],
     }),
     !isDev && terser(), // minify, but only in production
     isDev &&
       serve({
         open: true,
-        contentBase: ["dist"],
-        openPage: "/index.html",
-        historyApiFallback: "/index.html",
+        contentBase: ['dist'],
+        openPage: '/index.html',
+        historyApiFallback: '/index.html',
       }),
     isDev && livereload(),
   ],
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,8 @@
-import mzConsole from "./modules/test";
-console.log("version: {{version}}");
-console.log("fdfs");
+import mzConsole from './modules/test';
+
+console.log('version: {{version}}');
+console.log('fdfs');
 mzConsole();
+
+const peter = 'unused';
+console.log('peter', peter);
